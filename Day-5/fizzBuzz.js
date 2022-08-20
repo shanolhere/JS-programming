1762745e58b0283b5df45b75afc895a8ea4539f4
Shanol <102300466+shanolhere@users.noreply.github.com>
2022-08-20 15:39:00 +0530

    update: day-5 aug 20

--- Day-5/fizzBuzz.js
+++ Day-5/fizzBuzz.js
@@ -1,4 +1,27 @@
+/*Method-1
+
 let n=100;
 for(let i=1;i<=n;i++){
-  console.log(i,  i%3===0 && i%5==0 ?"FIZZBUZZ" : i%3===0? "FIZZ" : i%5===0?"BUZZ" :"")
+  console.log(i,  i%3===0 && i%5===0 ?"FIZZBUZZ" : i%3===0? "FIZZ" : i%5===0?"BUZZ" :"")
 }
+*/
+
+
+//Method-2
+function fizzBuzz(n){
+  for(let i=1;i<=n;i++){
+    if(i%3===0 && i%5===0){
+      console.log(i, "FIZZBUZZ")
+    }
+    else if(i%3===0){
+      console.log(i, "FIZZ")
+    }
+    else if(i%5===0){
+      console.log(i,"BUZZ")
+    }
+    else{
+      console.log(i)
+    }
+  }
+}
+fizzBuzz(100)
